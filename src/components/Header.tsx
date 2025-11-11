1d3d91813dd790a9a3a4d985dc2946e85e3574ff
EdsonnOliveira <edsonpinheiroliveira@gmail.com>
2025-11-11 09:39:07 -0300

    fix: remove comercial menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,14 +15,19 @@ const menuItems: MenuItem[] = [
     icon: 'M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2H5a2 2 0 00-2-2z'
   },
   {
-    label: 'Comercial',
-    name: '/commercial',
-    icon: 'M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z',
-    children: [
-      { label: 'Vendas', name: '/sales', icon: 'M13 7h8m0 0v8m0-8l-8 8-4-4-6 6' },
-      { label: 'Propostas', name: '/proposals', icon: 'M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z' }
-    ]
+    label: 'Vendas',
+    name: '/sales',
+    icon: 'M13 7h8m0 0v8m0-8l-8 8-4-4-6 6'
   },
+  // {
+  //   label: 'Comercial',
+  //   name: '/commercial',
+  //   icon: 'M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z',
+  //   children: [
+  //     { label: 'Vendas', name: '/sales', icon: 'M13 7h8m0 0v8m0-8l-8 8-4-4-6 6' },
+  //     { label: 'Propostas', name: '/proposals', icon: 'M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z' }
+  //   ]
+  // },
   {
     label: 'CRM',
     name: '/crm',
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -29,14 +29,19 @@ const menuItems: MenuItem[] = [
     icon: 'M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z'
   },
   {
-    label: 'Comercial',
-    name: '/commercial',
-    icon: 'M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z',
-    children: [
-      { label: 'Vendas', name: '/sales', icon: 'M13 7h8m0 0v8m0-8l-8 8-4-4-6 6' },
-      { label: 'Propostas', name: '/proposals', icon: 'M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z' }
-    ]
+    label: 'Vendas',
+    name: '/sales',
+    icon: 'M13 7h8m0 0v8m0-8l-8 8-4-4-6 6'
   },
+  // {
+  //   label: 'Comercial',
+  //   name: '/commercial',
+  //   icon: 'M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z',
+  //   children: [
+  //     { label: 'Vendas', name: '/sales', icon: 'M13 7h8m0 0v8m0-8l-8 8-4-4-6 6' },
+  //     { label: 'Propostas', name: '/proposals', icon: 'M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z' }
+  //   ]
+  // },
   {
     label: 'Integrador',
     name: '/integrator',
